b1aad17e3ec739651186ad8e94aac4bd3504177c
Palkiayp <87257272+AnthonyPark465@users.noreply.github.com>
2025-01-30 18:48:20 +0800

    pull

--- Scripts/crate.gd
+++ Scripts/crate.gd
@@ -3,10 +3,16 @@ extends StaticBody2D  # The crate does NOT move
 @export var health: int = 2  # Number of hits before breaking
 @onready var sprite = $Sprite2D  # Reference to the crate sprite
 
+func _ready():
+	add_to_group("attachable")  # Ensure this object can be attached
+	print("Crate ready! Health:", health)
+
 func break_crate():
 	health -= 1
+	print("Crate hit! Remaining health:", health)
+
 	if health <= 0:
 		queue_free()  # Remove the crate from the game
-		print("Crate destroyed!")  # Debug message
+		print("Crate destroyed!")
 	else:
 		sprite.modulate = Color(1, 0.5, 0.5)  # Change color to show damage
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -18,6 +18,10 @@ var moves = []
 # Stores the grid size, which is 16 (same as one tile)
 var grid_size = 16
 
+# Tracks the attached object (e.g., crate)
+var attached_object = null
+var attachment_offset = Vector2.ZERO
+
 func _process(_delta):
 	if Input.is_action_pressed("Right"):
 		sprite_map.play("right")
@@ -25,11 +29,14 @@ func _process(_delta):
 		sprite_map.play("left")
 	else:
 		sprite_map.play("default")
-
+	
+	# Consistently check for Q (ui_cancel) input when an object is attached
+	if attached_object and Input.is_action_pressed("detach_object"):
+		detach_object()
 
 # Calls the move function with the appropriate input key
 # if any input map action is triggered
-func _unhandled_input(event):
+func _input(event):
 	for action in inputs.keys():
 		if event.is_action_pressed(action):
 			move(action, true)
@@ -41,6 +48,10 @@ func _unhandled_input(event):
 	# Restart
 	if event.is_action("Restart"):
 		restart()
+	
+	# Attach object
+	if event.is_action_pressed("ui_accept"):
+		attempt_attachment()
 
 # Updates the direction of the RayCast2D according to the input key
 # and moves one grid if no collision is detected
@@ -48,11 +59,48 @@ func move(action, newMove):
 	var destination = inputs[action] * grid_size
 	ray_cast_2d.target_position = destination
 	ray_cast_2d.force_raycast_update()
-	if not ray_cast_2d.is_colliding():
-		position += destination
+	
+	if attached_object:
+		var new_player_pos = position + destination
+		var new_object_pos = attached_object.position + attachment_offset
 		
-		if (newMove):
-			moves.append(destination);
+		if not ray_cast_2d.is_colliding():
+			position = new_player_pos
+			attached_object.position = new_player_pos + attachment_offset
+			
+			if newMove:
+				moves.append(destination)
+	else:
+		if not ray_cast_2d.is_colliding():
+			position += destination
+			if newMove:
+				moves.append(destination)
+
+func attempt_attachment():
+	var directions = {
+		"right": Vector2.RIGHT,
+		"left": Vector2.LEFT,
+		"down": Vector2.DOWN,
+		"up": Vector2.UP
+	}
+	
+	for dir in directions.keys():
+		ray_cast_2d.target_position = directions[dir] * grid_size
+		ray_cast_2d.force_raycast_update()
+		
+		if ray_cast_2d.is_colliding():
+			var collider = ray_cast_2d.get_collider()
+			
+			if collider.is_in_group("attachable"):
+				attached_object = collider
+				attachment_offset = directions[dir] * grid_size
+				return
+
+func detach_object():
+	if attached_object:
+		attached_object.global_position = position + attachment_offset  # Keep the object in its place
+		attached_object = null
+		attachment_offset = Vector2.ZERO
 
 func undo():
 	if (!moves.is_empty()):
