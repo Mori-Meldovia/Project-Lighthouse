666baecabe407605dc74181f026c0459b759076d
Ebbflowo <129880057+Mori-Meldovia@users.noreply.github.com>
2025-01-31 15:52:50 +0800

    animation fixed

--- Scripts/gear.gd
+++ Scripts/gear.gd
@@ -2,6 +2,8 @@ extends StaticBody2D
 
 @onready var self_sprite = $AnimatedSprite2D
 
+var latched = false;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self_sprite.play("default")
@@ -13,12 +15,16 @@ func _process(delta: float) -> void:
 
 func attached():
 	self_sprite.play("attached")
+	latched = true
+	
 
 func detached():
 	self_sprite.play("default")
+	latched = false
 
 func power_on():
-	self_sprite.play("powered")
+	if !latched:
+		self_sprite.play("powered")
 	
 func power_off():
 	self_sprite.play("default")
